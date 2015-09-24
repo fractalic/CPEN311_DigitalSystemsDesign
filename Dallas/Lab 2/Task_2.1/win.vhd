a459b5c267a28f7580164d03d060180fba458c3e
Dallas <ddgawlick@gmail.com>
2015-09-23 21:32:29 -0700

    Changed win.vhd
    Deleted first try; restarting in one process / without for loops.

--- Dallas/Lab 2/Task_2.1/win.vhd
+++ Dallas/Lab 2/Task_2.1/win.vhd
@@ -30,70 +30,34 @@ END win;
 ARCHITECTURE behavioural OF win IS
   
 BEGIN
-  bet1 : process
+  
+  process (all)
   begin
-    if (bet1_value = spin_result_latched) then
+    
+    if spin_result_latched = bet1_value then
       bet1_wins <= '1';
     else bet1_wins <= '0';
     end if;
-  end process;
-  
-  bet2 : process
-  type colour_array is array (0 to 17) of std_logic_vector(5 downto 0); -- array describing the different sets of colours
-  variable red : colour_array := ("000001", "000011", "000101", "000111", "001001", "001100", "001110", "010000", "010010", "010011", "010101", 
-                                  "010111", "011001", "011011", "011110", "100000", "100010", "100100");
-  variable black : colour_array := ("000010", "000100", "000110", "001000", "001010", "001011", "001101", "001111", "010001", "010100", "010110",
-                                    "011000", "011010", "011100", "011101", "011111", "100001", "100011");
-  begin
-    case bet2_colour is
-      when '1' => -- red
-        for i in 0 to 17 loop
-          if (spin_result_latched = red(i)) then -- runs through red variable, checks for a match
-            bet2_wins <= '1';
-          else bet2_wins <= '0';
-          end if;
-        end loop;
-      when '0' => -- black
-        for i in 0 to 17 loop
-          if (spin_result_latched = black(i)) then -- runs through black variable, checks for a match
-            bet2_wins <= '1';
-          else bet2_wins <= '0';
-          end if;
-        end loop;
-      when others => bet2_wins <= '0'; -- only a win is a win
-    end case;
-  end process;
-  
-  bet3 : process
-  type dozen_array is array (0 to 11) of std_logic_vector(5 downto 0);
-  variable first_dozen : dozen_array := ("000001", "000010", "000011", "000100", "000101", "000110", "000111", "001000", "001001", "001010", "001011", "001100");
-  variable second_dozen : dozen_array := ("001101", "001110", "001111", "010000", "010001", "010010", "010011", "010100", "010101", "010110", "010111", "011000");
-  variable third_dozen : dozen_array := ("011001", "011010", "011011", "011100", "011101", "011110", "011111", "100000", "100001", "100010", "100011", "100100");
-  begin
+    
     case bet3_dozen is
-      when "00" => -- first dozen
-        for i in 0 to 11 loop
-          if (spin_result_latched = first_dozen(i)) then -- checks whether the winning number is in first_dozen
-            bet3_wins <= '1';
-          else bet3_wins <= '0';
-          end if;
-        end loop;
-      when "01" => -- second dozen
-        for i in 0 to 11 loop
-          if (spin_result_latched = second_dozen(i)) then -- checks whether the winning number is in second_dozen
-            bet3_wins <= '1';
-          else bet3_wins <= '0';
-          end if;
-        end loop;
-      when "10" => -- third dozen
-        for i in 0 to 11 loop
-          if (spin_result_latched = third_dozen(i)) then -- checks whether the winning number is in third_dozen
-            bet3_wins <= '1';
-          else bet3_wins <= '0';
-          end if;
-        end loop;
-      when others => bet3_wins <= '0'; -- only a win is a win
+    when "00" => -- first dozen
+      if spin_result_latched < 13 then
+        bet3_wins <= '1';
+      else bet3_wins <= '0';
+      end if;
+    when "01" => -- second dozen
+      if spin_result_latched < 25 and spin_result_latched > 12 then
+        bet3_wins <= '1';
+      else bet3_wins <= '0';
+      end if;
+    when "10" => -- third dozen
+      if spin_result_latched < 37 and spin_result_latched > 24 then
+        bet3_wins <= '1';
+      else bet3_wins <= '0';
+      end if;
+    when others => bet3_wins <= '0'; -- only a win is a win
     end case;
+    
   end process;
   
 END;
