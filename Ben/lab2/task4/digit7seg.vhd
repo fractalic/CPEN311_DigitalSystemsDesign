b90b89721c09a7e22a1af9c895ac9d4d447ffbd6
Ben Hughes <benhughes@shaw.ca>
2015-09-26 23:23:43 -0700

    Completed task 4

--- Ben/lab2/task4/digit7seg.vhd
+++ Ben/lab2/task4/digit7seg.vhd
@@ -27,10 +27,27 @@ ARCHITECTURE behavioral OF digit7seg IS
 signal seg7_sig : std_logic_vector(6 downto 0);
 begin
 
+    seg7 <= seg7_sig;
+
     process(digit)
     begin
         case digit is
-            when to_unsigned(0, digit'length) => seg7_sig <= "1000000";
+            when to_unsigned(0, digit'length)  => seg7_sig <= "1000000";
+            when to_unsigned(1, digit'length)  => seg7_sig <= "1111001";
+            when to_unsigned(2, digit'length)  => seg7_sig <= "0100100";
+            when to_unsigned(3, digit'length)  => seg7_sig <= "0110000";
+            when to_unsigned(4, digit'length)  => seg7_sig <= "0011001";
+            when to_unsigned(5, digit'length)  => seg7_sig <= "0010010";
+            when to_unsigned(6, digit'length)  => seg7_sig <= "0000010";
+            when to_unsigned(7, digit'length)  => seg7_sig <= "1111000";
+            when to_unsigned(8, digit'length)  => seg7_sig <= "0000000";
+            when to_unsigned(9, digit'length)  => seg7_sig <= "0010000";
+            when to_unsigned(10, digit'length) => seg7_sig <= "0001000";
+            when to_unsigned(11, digit'length) => seg7_sig <= "0000111";
+            when to_unsigned(12, digit'length) => seg7_sig <= "1000110";
+            when to_unsigned(13, digit'length) => seg7_sig <= "0100001";
+            when to_unsigned(14, digit'length) => seg7_sig <= "0000110";
+            when to_unsigned(15, digit'length) => seg7_sig <= "0001110";
             when others => seg7_sig <= "0000000";
         end case;
     end process;
