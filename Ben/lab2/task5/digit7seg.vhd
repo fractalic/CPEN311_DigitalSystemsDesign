31c64f56722cd3c1c97a10991bd88172b7097bf1
Ben Hughes <benhughes@shaw.ca>
2015-09-27 00:06:52 -0700

    Added necessary files and created components

---
+++ Ben/lab2/task5/digit7seg.vhd
@@ -0,0 +1,49 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+LIBRARY WORK;
+USE WORK.ALL;
+
+---
+-- Convert a number to a human-readable pattern for a 7-segment display.
+-- effects: seg7_pattern is wired to a bit pattern representing the number.
+--
+ENTITY digit7seg IS
+    PORT(
+          hex_digit : IN  UNSIGNED(3 DOWNTO 0);  -- number 0 to 0xF
+          seg7_pattern : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)  -- one per segment
+    );
+END;
+
+
+ARCHITECTURE behavioral OF digit7seg IS
+signal seg7_sig : std_logic_vector(6 downto 0);
+begin
+
+    seg7_pattern <= seg7_sig;
+
+    process(hex_digit)
+    begin
+        case hex_digit is
+            when to_unsigned(0, hex_digit'length)  => seg7_sig <= "1000000";
+            when to_unsigned(1, hex_digit'length)  => seg7_sig <= "1111001";
+            when to_unsigned(2, hex_digit'length)  => seg7_sig <= "0100100";
+            when to_unsigned(3, hex_digit'length)  => seg7_sig <= "0110000";
+            when to_unsigned(4, hex_digit'length)  => seg7_sig <= "0011001";
+            when to_unsigned(5, hex_digit'length)  => seg7_sig <= "0010010";
+            when to_unsigned(6, hex_digit'length)  => seg7_sig <= "0000010";
+            when to_unsigned(7, hex_digit'length)  => seg7_sig <= "1111000";
+            when to_unsigned(8, hex_digit'length)  => seg7_sig <= "0000000";
+            when to_unsigned(9, hex_digit'length)  => seg7_sig <= "0010000";
+            when to_unsigned(10, hex_digit'length) => seg7_sig <= "0001000";
+            when to_unsigned(11, hex_digit'length) => seg7_sig <= "0000111";
+            when to_unsigned(12, hex_digit'length) => seg7_sig <= "1000110";
+            when to_unsigned(13, hex_digit'length) => seg7_sig <= "0100001";
+            when to_unsigned(14, hex_digit'length) => seg7_sig <= "0000110";
+            when to_unsigned(15, hex_digit'length) => seg7_sig <= "0001110";
+            when others => seg7_sig <= "0000000";
+        end case;
+    end process;
+
+end;
--- Ben/lab2/task5/roulette.vhd
+++ Ben/lab2/task5/roulette.vhd
@@ -5,35 +5,81 @@ USE IEEE.NUMERIC_STD.ALL;
 LIBRARY WORK;
 USE WORK.ALL;
 
-----------------------------------------------------------------------
---
---  This is the top level template for Lab 2.  Use the schematic on Page 3
---  of the lab handout to guide you in creating this structural description.
---  The combinational blocks have already been designed in previous tasks,
---  and the spinwheel block is given to you.  Your task is to combine these
---  blocks, as well as add the various registers shown on the schemetic, and
---  wire them up properly.  The result will be a roulette game you can play
---  on your DE2.
---
------------------------------------------------------------------------
 
-ENTITY roulette IS
-	PORT(   CLOCK_27 : IN STD_LOGIC; -- the fast clock for spinning wheel
-		KEY : IN STD_LOGIC_VECTOR(3 downto 0);  -- includes slow_clock and reset
-		SW : IN STD_LOGIC_VECTOR(17 downto 0);
-		LEDG : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);  -- ledg
-		HEX7 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 7
-		HEX6 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 6
-		HEX5 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 5
-		HEX4 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 4
-		HEX3 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 3
-		HEX2 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 2
-		HEX1 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 1
-		HEX0 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)   -- digit 0
+---
+-- A Roulette game for the DE2 board. Three bets are allowed on each round
+-- (one straight up, one colour, and one dozen). Money is paid out according
+-- to the European odds.
+-- requires: CLOCK_27 is wired to a fast clock running continuously.
+-- effects: HEX6 and HEX7 are wired to a two digit hexadecimal number representing
+--          the value of the last spin.
+--          HEX0 through HEX2 are wired to a three digit hexadecimal number
+--          representing the player's current total money.
+--          LEDG(0) is high if player wins on bet1.
+--          LEDG(1) is high if player wins on bet2.
+--          LEDG(2) is high if player wins on bet3.
+--          
+entity roulette is
+	port(   CLOCK_27 : in STD_LOGIC; -- the fast clock for spinning wheel
+		KEY : in STD_LOGIC_VECTOR(3 downto 0);  -- includes slow_clock and reset
+		SW : in STD_LOGIC_VECTOR(17 downto 0);
+		LEDG : out STD_LOGIC_VECTOR(3 DOWNTO 0);  -- ledg
+		HEX7 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 7
+		HEX6 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 6
+		HEX5 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 5
+		HEX4 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 4
+		HEX3 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 3
+		HEX2 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 2
+		HEX1 : out STD_LOGIC_VECTOR(6 DOWNTO 0);  -- digit 1
+		HEX0 : out STD_LOGIC_VECTOR(6 DOWNTO 0)   -- digit 0
 	);
-END roulette;
+end roulette;
 
 
-ARCHITECTURE structural OF roulette IS
- --- Your code goes here
-END;
+architecture structural of roulette is
+
+component new_balance is
+    port(
+        money     : in unsigned (11 downto 0);
+        value1    : in unsigned (2 downto 0);
+        value2    : in unsigned (2 downto 0);
+        value3    : in unsigned (2 downto 0);
+        bet1_wins : in std_logic;
+        bet2_wins : in std_logic;
+        bet3_wins : in std_logic;
+        new_money : out unsigned (11 downto 0)
+    );
+end component;
+
+component digit7seg is
+    port(
+          hex_digit     : in  unsigned(3 downto 0);
+          seg7_pattern  : out std_logic_vector(6 downto 0)
+    );
+end component;
+
+component win is
+    port(
+        spin_result_latched : in unsigned(5 downto 0);
+        bet1_value          : in unsigned(5 downto 0);
+        bet2_colour         : in std_logic;
+        bet3_dozen          : in unsigned(1 downto 0);
+        bet1_wins           : out std_logic;
+        bet2_wins           : out std_logic;
+        bet3_wins           : out std_logic
+    );
+end component;
+
+component spinwheel is
+    port(
+        fast_clock   : in  std_logic;
+        resetb       : in  std_logic; -- asynchronous
+        spin_result  : out unsigned(5 downto 0)
+    );
+end component;
+
+begin
+
+-- TODO: connect everything together
+
+end;
