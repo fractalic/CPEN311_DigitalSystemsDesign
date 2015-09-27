42e9e1107946d9b54bf85eaf6b8a67df3ea0b186
Ben Hughes <benhughes@shaw.ca>
2015-09-26 23:12:38 -0700

    Started task 4

--- Ben/lab2/task4/digit7seg.vhd
+++ Ben/lab2/task4/digit7seg.vhd
@@ -16,16 +16,23 @@ USE WORK.ALL;
 --------------------------------------------------------
 
 ENTITY digit7seg IS
-	PORT(
+    PORT(
           digit : IN  UNSIGNED(3 DOWNTO 0);  -- number 0 to 0xF
           seg7 : OUT STD_LOGIC_VECTOR(6 DOWNTO 0)  -- one per segment
-	);
+    );
 END;
 
 
 ARCHITECTURE behavioral OF digit7seg IS
-BEGIN
+signal seg7_sig : std_logic_vector(6 downto 0);
+begin
 
--- Your code goes here
+    process(digit)
+    begin
+        case digit is
+            when to_unsigned(0, digit'length) => seg7_sig <= "1000000";
+            when others => seg7_sig <= "0000000";
+        end case;
+    end process;
 
-END;
+end;
