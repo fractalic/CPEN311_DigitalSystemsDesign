56706b462072a39c9e66439a58de2a1447c57e4f
Ben Hughes <benhughes@shaw.ca>
2015-10-18 15:32:15 -0700

    Added all datapath registers.

--- Ben/lab3/task3/lab3.vhd
+++ Ben/lab3/task3/lab3.vhd
@@ -33,9 +33,99 @@ architecture rtl of lab3 is
                 : out std_logic);
   end component;
 
-  
+  signal load_colour,
+         load_sx,
+         load_dx,
+         load_x,
+         load_x0,
+         load_x1,
+         load_sy,
+         load_dy,
+         load_y,
+         load_y0,
+         load_y1       : std_logic;
 
 begin
+
+  -- Datapath registers.
+  process(stateClock)
+  begin
+    if (rising_edge(stateClock)) then
+      if (load_colour = '1') then
+        if (colour_graycode) then
+          colour <= graycode;
+        else
+          colour <= COLOUR_BLACK;
+        end if;
+      end if;
+
+      if (load_sx = '1') then
+        if (x0 < x1) then
+          sx <= to_signed(1,  sx'length);
+        else
+          sx <= to_signed(-1, sx'length);
+        end if;
+      end if;
+
+      if (load_sy = '1') then
+        if (y0 < y1) then
+          sy <= to_signed(1,  sy'length);
+        else
+          sy <= to_signed(-1, sy'length);
+        end if;
+      end if;
+
+      if (load_dx = '1') then
+        dx <= abs(signed(x1)-signed(x0));
+      end if;
+
+      if (load_dy = '1') then
+        dy <= abs(signed(y1)-signed(y0));
+      end if;
+
+      if (load_x = '1') then
+        if (clear_screen = '1') then
+          x <= i;
+        else
+          if (init_x = '1') then
+            x <= x0;
+          else
+            x <= x + sx;
+          end if;
+        end if;
+      end if;
+
+      if (load_y = '1') then
+        if (clear_screen = '1') then
+          y <= j;
+        else
+          if (init_y = '1') then
+            y <= y0;
+          else
+            y <= y + sy;
+          end if;
+        end if;
+      end if;
+
+      if (load_x0 = '1') then
+        x0 <= to_unsigned(0, x0'length);
+      end if;
+
+      if load_x1 = '1') then
+        x1 <= to_unsigned(159, x0'length);
+      end if;
+
+      if (load_y0 = '1') then
+        y0 <= to_unsigned(k,y0'length - 4)*to_unsigned(8, 4);
+      end if;
+
+      if (load_y1 = '1') then
+        y1 <= to_unsigned(120,y1'length)
+              - to_unsigned(k,y1'length - 4)*to_unsigned(8, 4)
+
+
+    end if;
+  end process;
 
 
   -- VGA Driver
