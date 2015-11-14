9555ab72f9d5b34a2140ecf67a1aa286a442d5d2
Dallas <ddgawlick@gmail.com>
2015-11-14 14:31:36 -0800

    Task 3 Done!
    Had to modify the variables - one for each note's left and right volume.
    A little tedious, but it works.

--- Dallas/Lab 5/Task 3/sound.vhd
+++ Dallas/Lab 5/Task 3/sound.vhd
@@ -69,7 +69,8 @@ BEGIN
 
    process (clock_50)
 	variable current_state : state;
-	variable temp_left, temp_right : unsigned(23 downto 0);
+	variable temp_left_c, temp_left_d, temp_left_e, temp_left_f, temp_left_g, temp_left_a, temp_left_b : unsigned(23 downto 0);
+	variable temp_right_c, temp_right_d, temp_right_e, temp_right_f, temp_right_g, temp_right_a, temp_right_b : unsigned(23 downto 0);
 	begin
 	if rising_edge(clock_50) then
 	case current_state is
@@ -87,11 +88,11 @@ BEGIN
 				count_C <= 0;
 			end if;
 			if count_C > (NUM_SAMPLES_C / 2) then -- if count_A > wavelength_A /2 then
-				temp_left := temp_left + VOLUME; -- variable_left := volume + variable_left
-				temp_right := temp_right + VOLUME;
+				temp_left_c := VOLUME;--temp_left + VOLUME; -- variable_left := volume + variable_left
+				temp_right_c := VOLUME;--temp_right + VOLUME;
 			else
-				temp_left := temp_left + NEG_VOLUME;
-				temp_right := temp_right + NEG_VOLUME;
+				temp_left_c := NEG_VOLUME;--temp_left + NEG_VOLUME;
+				temp_right_c := NEG_VOLUME;--temp_right + NEG_VOLUME;
 			end if;
 		end if;
 		if sw(5) = '1' then
@@ -100,20 +101,86 @@ BEGIN
 				count_D <= 0;
 			end if;
 			if count_D > (NUM_SAMPLES_D / 2) then
-				temp_left := temp_left + VOLUME; -- variable_left := volume + variable_left
-				temp_right := temp_right + VOLUME;
+				temp_left_d := VOLUME; 
+				temp_right_d := VOLUME;
 			else
-				temp_left := temp_left + NEG_VOLUME;
-				temp_right := temp_right + NEG_VOLUME;
+				temp_left_d := NEG_VOLUME;
+				temp_right_d := NEG_VOLUME;
+			end if;
+		end if;
+		if sw(4) = '1' then
+			count_E <= count_E + 1;
+			if count_E > NUM_SAMPLES_E then
+				count_E <= 0;
+			end if;
+			if count_E > (NUM_SAMPLES_E / 2) then
+				temp_left_e := VOLUME; 
+				temp_right_e := VOLUME;
+			else
+				temp_left_e := NEG_VOLUME;
+				temp_right_e := NEG_VOLUME;
+			end if;
+		end if;
+		if sw(3) = '1' then
+			count_F <= count_F + 1;
+			if count_F > NUM_SAMPLES_F then
+				count_F <= 0;
+			end if;
+			if count_F > (NUM_SAMPLES_F / 2) then
+				temp_left_f := VOLUME; 
+				temp_right_f := VOLUME;
+			else
+				temp_left_f := NEG_VOLUME;
+				temp_right_f := NEG_VOLUME;
+			end if;
+		end if;
+		if sw(2) = '1' then
+			count_G <= count_G + 1;
+			if count_G > NUM_SAMPLES_G then
+				count_G <= 0;
+			end if;
+			if count_G > (NUM_SAMPLES_G / 2) then
+				temp_left_g := VOLUME; 
+				temp_right_g := VOLUME;
+			else
+				temp_left_g := NEG_VOLUME;
+				temp_right_g := NEG_VOLUME;
+			end if;
+		end if;
+		if sw(1) = '1' then
+			count_A <= count_A + 1;
+			if count_A > NUM_SAMPLES_A then
+				count_A <= 0;
+			end if;
+			if count_A > (NUM_SAMPLES_A / 2) then
+				temp_left_a := VOLUME; 
+				temp_right_a := VOLUME;
+			else
+				temp_left_a := NEG_VOLUME;
+				temp_right_a := NEG_VOLUME;
+			end if;
+		end if;
+		if sw(0) = '1' then
+			count_B <= count_B + 1;
+			if count_B > NUM_SAMPLES_B then
+				count_B <= 0;
+			end if;
+			if count_B > (NUM_SAMPLES_B / 2) then
+				temp_left_b := VOLUME; 
+				temp_right_b := VOLUME;
+			else
+				temp_left_b := NEG_VOLUME;
+				temp_right_b := NEG_VOLUME;
 			end if;
 		end if;
 		write_s <= '1';
-		writedata_left <= std_logic_vector(temp_left);
-		writedata_right <= std_logic_vector(temp_right);
+		writedata_left <= std_logic_vector(temp_left_c + temp_left_d + temp_left_e + temp_left_f + temp_left_g + temp_left_a + temp_left_b);
+		writedata_right <= std_logic_vector(temp_right_c + temp_right_d + temp_right_e + temp_right_f + temp_right_g + temp_right_a + temp_right_b);
 		current_state := wait_state_2;
 		
 		when wait_state_2 =>
 		if write_ready = '0' then
+			write_s <= '0';
 			current_state := wait_state;
 		end if;
 
